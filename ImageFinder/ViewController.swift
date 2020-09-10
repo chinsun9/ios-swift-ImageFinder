399d0d41e65a3acac6fc7358d3d48dc202950492
sung <chin_sung@naver.com>
2020-09-10 10:28:56 +0900

    init

---
+++ ImageFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageFinder
+//
+//  Created by sung hello on 2020/09/10.
+//  Copyright © 2020 sung hello. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
