18d0afb90cc93a6d4a5922697520a64ba393d8af
sung <chin_sung@naver.com>
2020-09-14 09:46:00 +0900

    Update DetailImageViewController.swift
    - 디테일창 열었을때 이미지 보여주기와, 웹뷰 로딩하기
    - 웹뷰 스크롤하면 이미지 숨기기

--- ImageFinder/DetailImageViewController.swift
+++ ImageFinder/DetailImageViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 import WebKit
 
 
-class DetailImageViewController: UIViewController {
+class DetailImageViewController: UIViewController, UIScrollViewDelegate {
 
     var document: NSDictionary = [:]
     var url: String = ""
     var imageUrl: String = ""
+    var isHideImage: Bool = false
    
     
     @IBOutlet var myWebView: WKWebView!
@@ -24,12 +25,15 @@ class DetailImageViewController: UIViewController {
         super.viewDidLoad()
         
         viewWillDisappear(true)
+        myWebView.scrollView.delegate = self
         
         print(document)
         
         // 이미지 세팅
+        setImage()
         
         // 페이지 로드
+        setWeb()
         
 //        loadWebPateT##url: String##Strin)
     }
@@ -40,12 +44,45 @@ class DetailImageViewController: UIViewController {
            }
     
     
+    func setImage() {
+        imageView.downloaded(from: document.value(forKeyPath: "image_url") as! String)
+    }
+    
+    func setWeb() {
+        loadWebPate(document.value(forKeyPath: "doc_url") as! String)
+    }
+    
+    // 웹뷰 스크롤하면 이미지 감추기
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+//        print(scrollView.contentOffset.y)
+        if (!isHideImage && scrollView.contentOffset.y >= 120) {
+//            print(imageView.constraints)
+            isHideImage.toggle()
+            hideImage(isHideImage)
+        } else if (isHideImage && scrollView.contentOffset.y < 120) {
+            isHideImage.toggle()
+            hideImage(isHideImage)
+        }
+        
+    }
+    
+    func hideImage(_ isHide: Bool) {
+        imageView.layoutIfNeeded() // force any pending operations to finish
+
+        UIView.animate(withDuration: 0.5, animations: { () -> Void in
+            let height: CGFloat = isHide ? 0 : 400
+            self.imageView.constraints[2].constant = height
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+
+    
     
     func loadWebPate(_ url: String){
         let myUrl = URL(string: url)
         let myRequest = URLRequest(url:myUrl!)
         myWebView.load(myRequest)
-        
-        myWebView.addObserver(self, forKeyPath: #keyPath(WKWebView.isLoading), options: .new, context: nil)
     }
 }
